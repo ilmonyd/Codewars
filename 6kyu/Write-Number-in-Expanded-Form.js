08c041a8545da40dd5f03b0acb4cdeee9a3b2351
Oleg Semenov <olegsem23@gmail.com>
2021-06-16 16:21:54 +0200

    Update Write-Number-in-Expanded-Form.js

--- 6kyu/Write-Number-in-Expanded-Form.js
+++ 6kyu/Write-Number-in-Expanded-Form.js
@@ -1,1 +1,5 @@
-const expandedForm = num => num.toString().split('').map((val, i) => val += '0'.repeat(num.toString().length - i - 1)).filter(n => eval(n) !== 0).join(' + ');
+const expandedForm = num => num.toString()
+                                .split('')
+                                .map((val, i) => val += '0'.repeat(num.toString().length - i - 1))
+                                .filter(n => eval(n) !== 0)
+                                .join(' + ');
